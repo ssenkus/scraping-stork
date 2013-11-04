2962468612c763132b389559e5a5f11771c29cd8
Steven Senkus <steven.senkus@yahoo.com>
2013-11-03 17:17:58 -0800

    writes to a file!

--- scrape-stork.py
+++ scrape-stork.py
@@ -3,6 +3,9 @@ import mechanize
 from bs4 import BeautifulSoup
 
 os.system('cls')
+
+f = open("datafile.txt", "w")
+
 br = mechanize.Browser();
 url = "http://www.storkbabygiftbaskets.com/"
 response = br.open(url)
@@ -13,8 +16,16 @@ categories = [];
 print "Featured Gift Links:\n\n"
 for link in catLinks:
     href = link.get('href')
-    print href
+    text = link.string
+    print text + " -> " + href + "\n"
     categories.append(href)
-
-
+    resp = br.open(url + href)
+    soup = BeautifulSoup(resp)
+    breadcrumbContainer = soup.find(id="c4-breadcrumbs-id10T")
+    print breadcrumbContainer.b.prettify()
+    f.write(text + " -> " + href + "\n")
+    f.write(str(breadcrumbContainer) + "\n\n")
+    
+    
 raw_input("\n\nPress Enter to continue...\n\n")
+f.close();
